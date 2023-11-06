a608816fe34d5d08dd00aafdd3aabf5ba5022595
Karun <karun@tecorb.co>
2023-11-06 20:04:58 +0530

    authentication api added

--- src/graphql/users/resolvers.ts
+++ src/graphql/users/resolvers.ts
@@ -1,7 +1,15 @@
-const queries = {}
+import UserService, { CreateUserPayload, GetUserTokenPayload } from "../../services/users.js"
+
+const queries = {
+    getUserToken: async(_: any, payload: GetUserTokenPayload) => {
+        const resp = await UserService.getUserToken(payload)
+        return resp;
+    }
+}
 const mutations = {
-    createUser: async(_, any, {}:{}) => {
-        return 'id'
+    createUser: async(_: any, payload: CreateUserPayload) => {
+        const result = await UserService.createUser(payload)
+        return result.id
     }
 }
 
---
+++ src/services/users.ts
@@ -0,0 +1,63 @@
+import { createHmac, randomBytes } from 'node:crypto';
+import { prismaClient } from '../lib/db.js';
+import JWT from "jsonwebtoken"
+const TOKEN = "c449c786743b9b502a43b3374c0157c2"
+
+export interface CreateUserPayload {
+    firstName: string,
+    lastName?: string,
+    email: string,
+    password: string 
+}
+
+export interface GetUserTokenPayload {
+    email: string,
+    password: string 
+}
+
+class UserService {
+    private static generateHashPassword(salt: string, password: string) {
+        return createHmac('sha256', salt)
+        .update(password)
+        .digest("hex")
+    }
+
+    public static createUser(payload: CreateUserPayload) {
+        const { firstName, lastName, email, password } = payload
+        const salt = randomBytes(16).toString("hex");
+        const hashPassword = UserService.generateHashPassword(salt, password)
+
+        return prismaClient.user.create({
+            data: {
+                firstName,
+                lastName,
+                email,
+                salt,
+                password: hashPassword
+            }
+        })
+
+    }
+
+    private static getUserByEmail(email: string) {
+        return prismaClient.user.findUnique({ where: { email }})
+    }
+
+    public static async getUserToken(payload: GetUserTokenPayload) {
+        const { email, password } = payload;
+        const user = await UserService.getUserByEmail(email)
+        if(!user) throw new Error("User not found!")
+        const userSalt = user.salt;
+        const hashPassword = UserService.generateHashPassword(userSalt, password)
+        if(hashPassword == user.password) {
+            return JWT.sign({
+                id: user.id,
+                email: user.email
+            }, TOKEN)
+        } else {
+            throw new Error("Password is incorrect!")
+        }
+    }
+}
+
+export default UserService
